e8249ccd1341f17d5057203551806de797fbcd9c
sebas arriola <sebdeveloper6952@gmail.com>
2023-12-03 18:20:32 +0800

    remove unused file

--- example/lib/get_public_key.dart
+++
@@ -1,9 +0,0 @@
-import 'package:amberflutter/amberflutter.dart';
-
-Future<void> getPublicKey() async {
-  final amberflutterPlugin = Amberflutter();
-
-  String? pk = await amberflutterPlugin.getPublicKey();
-  // if user approves request in amber:
-  // pk = npub...
-}
